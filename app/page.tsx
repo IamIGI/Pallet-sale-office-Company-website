414d195781c3a50d608b3a3715295cd5d77f777d
i.kłusek <J6ZkqwVUn>
2024-02-04 21:59:54 +0100

    PFW-05-AboutUs section and RWD to 750px

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import AboutUsSection from '@/components/AboutUsSection/AboutUsSection';
 import ContactUsSection from '@/components/ContactUsSection/ContactUsSection';
 import MainHeadSection from '@/components/MainSection/MainHeadSection/MainHeadSection';
 import OfferSection from '@/components/OfferSection/OfferSection';
+import sectionsData from '@/assets/sectionsText/sectionsText.json';
+import { SectionModel, Sections } from '@/models/sectionModel';
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div className="home-page-wrapper">
       <MainHeadSection />
       <div className="home-page-section-wrapper">
-        <AboutUsSection />
+        {/* {sectionsData.find((data) => data.section === 'AboutUs')!.section} */}
+        <AboutUsSection
+          data={
+            (sectionsData as SectionModel[]).find(
+              (data) => data.section === Sections.AboutUs
+            )!
+          }
+        />
         <OfferSection />
         <ContactUsSection />
       </div>
--- components/AboutUsSection/AboutUsSection.tsx
+++ components/AboutUsSection/AboutUsSection.tsx
@@ -1,7 +1,15 @@
+import { SectionModel } from '@/models/sectionModel';
 import SectionBackground from '../SVGBackground/SVGBackground';
 import classes from './AboutUsSection.module.scss';
+import calendar from '@/assets/icons/Callendar.svg';
+import gears from '@/assets/icons/Gears.svg';
+import truck from '@/assets/icons/Truck.svg';
+import certificate from '@/assets/icons/Certificats.svg';
+import Image from 'next/image';
 
-export default function AboutUsSection() {
+export default function AboutUsSection({ data }: { data: SectionModel }) {
+  const icons = [calendar, gears, truck, certificate];
+
   return (
     <div className={classes.wrapper}>
       <SectionBackground>
@@ -15,8 +23,30 @@ export default function AboutUsSection() {
           <path d="M0 0H1724V544L439.525 386.278L0 0Z" fill="currentColor" />
         </svg>
       </SectionBackground>
-
-      <h1>AboutUs</h1>
+      <div className={classes.contentWrapper}>
+        <div className={classes.titleWrapper}>
+          <h1>{data.title}</h1>
+          <div className={classes.line} />
+        </div>
+        <div className={classes.innerWrapper}>
+          <article className={classes.article}>
+            <h2>{data.excerpt}</h2>
+            <p dangerouslySetInnerHTML={{ __html: data.description }} />
+          </article>
+          <div className={classes.tileWrapper}>
+            {data.tilesText.map((text, index) => (
+              <div className={classes.tile} key={index}>
+                <div className={classes.tileBackground}>
+                  <div className={classes.iconWrapper}>
+                    <Image src={icons[index]} alt={text} fill />
+                  </div>
+                </div>
+                <h3 dangerouslySetInnerHTML={{ __html: text }} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
