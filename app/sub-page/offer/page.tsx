1d7b2ae067c601e6a471bd4744952d6d82f4ca7a
i.kłusek <J6ZkqwVUn>
2024-02-19 18:46:50 +0100

    PFW-13-Offer page

--- app/sub-page/offer/page.tsx
+++ app/sub-page/offer/page.tsx
@@ -1,5 +1,50 @@
+import OfferList from '@/components/OfferList/OfferList';
 import classes from './page.module.scss';
+import data from '@/assets/data/offerPage.json';
+import Button from '@/components/Button/Button';
+import sendIcon from '@/assets/icons/SendMessage.svg';
+import infoIcon from '@/assets/icons/infoIcon.svg';
+import Image from 'next/image';
 
 export default function OfferPage() {
-  return <h1>Offer Page</h1>;
+  return (
+    <div className={classes.wrapper}>
+      <section className={classes.upperWrapper}>
+        <h1>{data.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: data.description }} />
+      </section>
+      <section className={classes.mainWrapper}>
+        <div className={classes.subTitleWrapper}>
+          <h2>{data.subtitle_1}</h2> <div className={classes.line} />
+        </div>
+        <section className={classes.mainContent}>
+          <div className={classes.leftWrapper}>
+            <OfferList data={data.offer} />
+          </div>
+          <div className={classes.rightWrapper}>
+            <div className={classes.hintWrapper}>
+              <p>
+                <Image src={infoIcon} alt="info" width="19" height="19" />
+                {data.hint}
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section className={classes.bottomWrapper}>
+        <p
+          className={classes.subTitleDesc}
+          dangerouslySetInnerHTML={{ __html: data.subtitle_1_desc }}
+        />
+        <div className={classes.buttonWrapper}>
+          <p>{data.subtitle_2} </p>
+          <Button
+            text={data.button_text}
+            href="'/sub-page//contact'"
+            icon={sendIcon}
+          ></Button>
+        </div>
+      </section>
+    </div>
+  );
 }
